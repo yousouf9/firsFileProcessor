19127e8ff68b836b41f36ccb65a8673dba4a7c51
ibrahim yusuf <ibraheemyousouf@gmail.com>
2021-11-07 22:57:11 +0100

    updated file directory temp

--- routes/index.js
+++ routes/index.js
@@ -366,11 +366,13 @@ function deleteAllTempFiles(directory){
       if (err) throw err;
 
       for (const file of files) {
-        fs.unlink(path.join(directory, file), err => {
-          if (err) throw err;
-
-          console.log("done!!");
-        });
+        if(file !== "test.jpg"){
+          fs.unlink(path.join(directory, file), err => {
+            if (err) throw err;
+  
+            console.log("done!!");
+          });
+        }
       }
     });
 }

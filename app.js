45c9a0f1f2f00f9338277f6fae96706ce22f400a
ibrahim yusuf <ibraheemyousouf@gmail.com>
2021-11-19 17:21:57 +0100

    updated

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ var createError = require('http-errors');
 const compression = require('compression');
 const cors = require('cors');
 var express = require('express');
+const winston = require('winston');
+const { format } = require('winston');
+  // require('winston-mongodb');
+  require('express-async-errors');
 var path = require('path');
 const session = require('express-session');
 const flash = require('connect-flash');
@@ -19,6 +23,42 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 app.use(logger('dev'));
+const logger1 = winston.createLogger({
+
+  format:format.combine(
+      winston.format.json(),
+      format.prettyPrint()
+  ),
+  defaultMeta: { service: 'user-service' },
+  transports:[
+      new winston.transports.File({ filename: 'error.log', level: 'error', }),
+      new winston.transports.File({ filename: 'uncaughtExceptions.log', handleExceptions:true, }),
+  ],
+  
+})
+
+
+
+process.on('unhandledRejection', (ex) => {
+  throw ex;
+});
+
+
+//check if the app is running on developement mode and print errors on the console
+
+if (process.env.NODE_ENV !== 'production') {
+  winston.add(new winston.transports.Console({
+  format: format.combine(
+      format.colorize(),
+      format.simple(),
+      format.prettyPrint(),
+      format.timestamp(),
+  ),
+
+  }));
+}
+
+winston.add(new winston.transports.File({ filename: 'logfile.log' }));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -58,7 +98,7 @@ app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
+  winston.error(err.message, err);
   // render the error page
   res.status(err.status || 500);
   res.render('error');
